aed6e00e6c1d5815e42ca877a4a986e7cf0deea2
Sarthak Gupta <sarthakgupta2537@gmail.com>
2024-03-24 20:57:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-let num1 = 8;
-let num2 = 2;
-document.getElementById("num1-el").textContent = num1;
-document.getElementById("num2-el").textContent = num2;
+"use strict";
 
-let finalAns = document.getElementById("sum-el");
-function add() {
-  finalAns.textContent = "Sum: ";
-  finalAns.textContent += num1 + num2;
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
 }
 
-function subtract() {
-  finalAns.textContent = "Difference: ";
-  finalAns.textContent += num1 - num2;
+function clearDisplay() {
+  display.value = "";
 }
 
-function divide() {
-  finalAns.textContent = "Division: ";
-  finalAns.textContent += num1 / num2;
-}
-
-function multiply() {
-  finalAns.textContent = "Multiplication: ";
-  finalAns.textContent += num1 * num2;
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (err) {
+    display.value = "Error";
+  }
 }
